8ee3c2ddc89981d073c6ffbf9ab6f3b381e0a1ce
arepina <anastasiya.repina2012@yandex.ru>
2020-04-18 20:55:41 +0300

    Added notifications hider

---
+++ Assets/Scripts/Notification/NotificationHider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NotificationHider : MonoBehaviour
+{
+    public float hideTimeOfTheNotificationAfterArrival;
+
+    void Start()
+    {
+        if (transform.parent.name != "TrayHolder")
+        {
+            StartCoroutine(Destroyer());
+        }
+    }
+
+    IEnumerator Destroyer()
+    {
+        yield return new WaitForSeconds(hideTimeOfTheNotificationAfterArrival);
+        Destroy(gameObject);
+    }
+}
