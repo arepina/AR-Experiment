ae0b90ec28855f7fde3f879b6efc5830329732cc
arepina <anastasiya.repina2012@yandex.ru>
2020-04-25 16:41:59 +0300

    Changed menu actions

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,16 +6,9 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
     public GameObject MainMenuPanel;
-
-    public Button[] ButtonsDependentOnTrackPosition;
-    public Button RestoreLastSessionButton;
-
     public GameObject InfoMessage;
-
     public InputField IMSubjectCodeInputField;
-    public InputField ESSubjectCodeInputField;
-    public ToggleGroup IntensityToggleGroup;
-    public ToggleGroup OrderToggleGroup;
+    public Text headerText;
 
     void Start()
     {
@@ -25,20 +16,6 @@ public class MenuController : MonoBehaviour
         if (MainMenuPanel == null)
         {
             Debug.LogError("Error: The MainMenuPanel field can't be left unassigned. Disabling the script");
-            enabled = false;
-            return;
-        }
-
-        if (ButtonsDependentOnTrackPosition.Length == 0)
-        {
-            Debug.LogError("Error: The 'Intencities' array can't be empty. Disabling the script");
-            enabled = false;
-            return;
-        }
-
-        if (RestoreLastSessionButton == null)
-        {
-            Debug.LogError("Error: The RestoreLastSessionButton field can't be left unassigned. Disabling the script");
             enabled = false;
             return;
         }
@@ -56,62 +33,53 @@ public class MenuController : MonoBehaviour
             enabled = false;
             return;
         }
-
-        if (IntensityToggleGroup == null)
-        {
-            Debug.LogError("Error: The IntensityToggleGroup field can't be left unassigned. Disabling the script");
-            enabled = false;
-            return;
-        }
-
-        if (OrderToggleGroup == null)
-        {
-            Debug.LogError("Error: The OrderToggleGroup field can't be left unassigned. Disabling the script");
-            enabled = false;
-            return;
-        }
     }
 
     public void StartNewSession()
     {
         try
         {
-            int subjectNo = Int32.Parse(ESSubjectCodeInputField.text);
-            IEnumerator<Toggle> enumerator = IntensityToggleGroup.ActiveToggles().GetEnumerator();
-            enumerator.MoveNext();
-            float intensityOfObstacleAppearance = float.Parse(enumerator.Current.name, CultureInfo.InvariantCulture.NumberFormat);
-            enumerator = OrderToggleGroup.ActiveToggles().GetEnumerator();
-            enumerator.MoveNext();
-            string orderOfDesignPresentation = enumerator.Current.name;
-
-            PlayerPrefs.SetInt("SubjectNo", subjectNo);
-            PlayerPrefs.SetFloat("Intensity", intensityOfObstacleAppearance);
-            PlayerPrefs.SetString("ConditionsOrder", orderOfDesignPresentation);
-
-            PlayerPrefs.SetInt("ConditionNo", 0);
-            PlayerPrefs.SetInt("TrialNo", 1);
+            //todo
+            //int subjectNo = Int32.Parse(ESSubjectCodeInputField.text);
+            //PlayerPrefs.SetInt("SubjectNo", subjectNo);
+            //PlayerPrefs.SetInt("ConditionNo", 0);
+            //PlayerPrefs.SetInt("TrialNo", 1);
+            string text = headerText.text.Split(':')[1].Trim();
+            switch (text)
+            {
+                case "Перед пользователем - мобильные":
+                    {
+                        StartInFrontOfMobile();
+                        return;
+                    }
+                case "Перед пользователем - стикеры":
+                    {
+                        StartInFrontOfStickers();
+                        return;
+                    }
+                case "Вокруг пользователя - мобильные":
+                    {
+                        StartAroundMobile();
+                        return;
+                    }
+                case "Вокруг пользователя - стикеры":
+                    {
+                        StartAroundStickers();
+                        return;
+                    }
+                case "Невидимые - волны":
+                    {
+                        StartHiddenWaves();
+                        return;
+                    }
+                default: return;
+            }
         }
         catch (Exception e)
         {
             Debug.LogException(e);
         }
-
-        SceneManager.LoadSceneAsync("ExperimentSession");
     }
-
-    public void MeasureIntensity()
-    {
-        int subjectNo = Int32.Parse(IMSubjectCodeInputField.text);
-        PlayerPrefs.SetInt("SubjectNo", subjectNo);
-
-        PlayerPrefs.DeleteKey("Intensity");
-        PlayerPrefs.DeleteKey("ConditionsOrder");
-        PlayerPrefs.DeleteKey("ConditionNo");
-        PlayerPrefs.DeleteKey("TrialNo");
-
-        SceneManager.LoadSceneAsync("IntensityMeasurement");
-    }
-
 
     public void StartInFrontOfMobile()
     {
